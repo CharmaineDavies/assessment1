a9a3adbf68a2eb3c7c679380e00e9d156090896d
CharmaineDavies <74466556+CharmaineDavies@users.noreply.github.com>
2022-12-06 12:34:34 +0100

    Loop made and funtioning

--- assess1.py
+++ assess1.py
@@ -1,41 +1,50 @@
 import psycopg2
+
+print("Welcome to the phone list, the following commands are available:")
+print("LIST, ADD, DELETE, QUIT")
+
 conn = psycopg2.connect(
    host="localhost",
+   port="5432",
    database="assessmentdb",
-   user="user",
+   user="postgres",
    password="Philip1986??"
 )
 
-def read_dict(C):
-    cur = C.cursor()
-    cur.execute("SELECT id, word, translation FROM dictionary;")
+def read_dict(conn):
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM contacts;")
     rows = cur.fetchall()
     cur.close()
     return rows
-def add_word(C, word, translation):
-    cur = C.cursor()
-    cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
+def add_word(conn, id, first_name, last_name, title, organization):
+    cur = conn.cursor()
+    cur.execute(f"INSERT INTO contacts (id, first_name, last_name, title, organization) VALUES ('{id}', '{first_name}', '{last_name}', '{title}', '{organization}');")
     cur.close()
-def delete_word(C, ID):
-    cur = C.cursor()
-    cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
+    print(f"{first_name} added!")
+def delete_word(conn, first_name):
+    cur = conn.cursor()
+    cur.execute(f"DELETE FROM contacts WHERE id = '{first_name}';")
     cur.close()
 def save_dict(C):
-    cur = C.cursor()
+    cur = conn.cursor()
     cur.execute("COMMIT;")
     cur.close()
 
-while True: ## REPL - Read Execute Program Loop
+while True: 
     cmd = input("Command: ")
-    if cmd == "list":
+    if cmd == "LIST":
         print(read_dict(conn))
-    elif cmd == "add":
-        name = input("  Word: ")
-        phone = input("  Translation: ")
-        add_word(conn, name, phone)
-    elif cmd == "delete":
-        ID = input("  ID: ")
-        delete_word(conn, ID)
-    elif cmd == "quit":
+    elif cmd == "ADD":
+        id = int(input("  Id: "))
+        first_name = input("  First name: ")
+        last_name = input("  Last name: ")
+        title = input("  Title: ")
+        organization = input("  Organization: ")
+        add_word(conn, id, first_name, last_name, title, organization)
+    elif cmd == "DELETE":
+        first_name = input("  First name: ")
+        delete_word(conn, first_name)
+    elif cmd == "QUIT":
         save_dict(conn)
         exit()
